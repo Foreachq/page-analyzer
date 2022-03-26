30822f0dd459bfe5bde5098b26a66ad423a42566
Ilya Golovin <foreachq@gmail.com>
2022-03-27 00:09:27 +0300

    add: UrlController tests

--- tests/Feature/UrlControllerTest.php
+++ tests/Feature/UrlControllerTest.php
@@ -2,9 +2,8 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Artisan;
+use App\Models\Url;
+use App\Repositories\UrlRepository;
 use Tests\TestCase;
 
 class UrlControllerTest extends TestCase
@@ -14,8 +13,77 @@ class UrlControllerTest extends TestCase
      *
      * @return void
      */
-    public function testSubmitNewUrl()
+    protected function setUp(): void
     {
-        $this->assertDatabaseCount('urls', 0);
+        parent::setUp();
+        $repo = new UrlRepository();
+
+        $url = new Url('https://laravel.com');
+        $repo->save($url);
+
+        $url = new Url('https://google.com');
+        $repo->save($url);
+    }
+
+    public function testExistingIndex()
+    {
+        $response = $this->get(route('urls.index', 1));
+
+        $response->assertOk();
+    }
+
+    public function testInvalidIndex()
+    {
+        $responseNumber = $this->get(route('urls.index', 999));
+        $responseNumber->assertNotFound();
+
+        $responseSymbol = $this->get(route('urls.index', 'abc'));
+        $responseSymbol->assertNotFound();
+    }
+
+    public function testShow()
+    {
+        $response = $this->get(route('urls'));
+
+        $response->assertOk();
+    }
+
+    public function testSubmitUrl()
+    {
+        $this->post(route('urls.create'), [
+            'url' => [
+                'name' => 'https://github.com'
+            ]
+        ]);
+
+        $this->assertDatabaseHas('urls', [
+            'name' => 'https://github.com'
+        ]);
+    }
+
+    public function testSubmitExistingUrl()
+    {
+        $this->post(route('urls.create'), [
+            'url' => [
+                // laravel.com was added in setUp()
+                'name' => 'https://laravel.com'
+            ]
+        ]);
+
+        // 2 sites from setUp()
+        $this->assertDatabaseCount('urls', 2);
+    }
+
+    public function testSubmitInvalidUrl()
+    {
+        $this->post(route('urls.create'), [
+            'url' => [
+                'name' => 'hts://github.com'
+            ]
+        ]);
+
+        $this->assertDatabaseMissing('urls', [
+            'name' =>  'hts://github.com'
+        ]);
     }
 }
